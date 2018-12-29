ea79825fa1556d8f08ec62f1946ab9cb4bdd663a
Urmat Bekboev <bek.urmat@gmail.com>
2018-12-29 13:45:01 +0600

    Added MENU title, style refactor made

--- src/components/AddItems/AddItems.js
+++ src/components/AddItems/AddItems.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import './AddItems.css';
 
-const MENU = [
-    {name: 'Hamburger', type: 'food', cost: 50},
-    {name: 'Cheeseburger', type: 'food', cost: 50},
-    {name: 'Fries', type: 'food', cost: 50},
-    {name: 'Coffee', type: 'drink', cost: 50},
+export const MENU = [
+    {name: 'Hamburger', type: 'food', cost: 80},
+    {name: 'Coffee', type: 'drink', cost: 70},
+    {name: 'Cheeseburger', type: 'food', cost: 90},
     {name: 'Tea', type: 'drink', cost: 50},
-    {name: 'Cola', type: 'drink', cost: 50}
+    {name: 'Fries', type: 'food', cost: 45},
+    {name: 'Cola', type: 'drink', cost: 40}
 ];
 
-const AddItems = () => {
+const AddItems = ({addOrder}) => {
     return (
         <React.Fragment>
             {MENU.map((item, index) => {
               return (
-                  <div className={"item " + item.type} key={index}>
+                  <div className={"item " + item.type} key={index} onClick={() => addOrder(item.name)}>
                       <h4>{item.name}</h4>
                       <p>Price: {item.cost} KGS</p>
                   </div>
