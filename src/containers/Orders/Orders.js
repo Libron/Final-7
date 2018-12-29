b8d3dbcf73e1ef706c4cb5aba044c190868cc4e8
Urmat Bekboev <bek.urmat@gmail.com>
2018-12-29 13:08:54 +0600

    Added Orders container

---
+++ src/containers/Orders/Orders.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import {MENU} from "../../components/AddItems/AddItems";
+
+class Orders extends Component {
+    render() {
+        let orders = [];
+        let totalCost = this.props.totalCost;
+        if (this.props.orders.length) {
+            orders = this.props.orders.map((item, index) => {
+                if (item.qty !== 0) {
+                    const menuIndex = MENU.findIndex(x => x.name===item.name);
+                    totalCost += item.qty * MENU[menuIndex].cost;
+                    return (
+                        <li className="order" key={index}>{item.name} <p> <span className="qty">x{item.qty}</span> {MENU[menuIndex].cost} KGS <button onClick={() => this.props.removeOrder(index)}> </button></p> </li>
+                    );
+                }
+                return orders = 'You cleaned list  of orders, please add something !';
+            });
+        } else {
+            orders = 'Order is empty !';
+        }
+
+        return (
+            <React.Fragment>
+                <ol>{orders}</ol>
+                <p className="total">Общая стоимость заказа: {totalCost}</p>
+            </React.Fragment>
+        );
+    }
+}
+
+export default Orders;
